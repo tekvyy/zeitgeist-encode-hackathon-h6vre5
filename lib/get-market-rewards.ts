faf0943fe4108eb2f0f017186918679bd89d22ad
tekvyy <divgun.gta2@gmail.com>
2023-06-29 11:34:50 +0200

    Added market data api

--- lib/get-market-rewards.ts
+++ lib/get-market-rewards.ts
@@ -1,3 +1,4 @@
+import { gql } from 'graphql-request';
 import client from './graphql-client';
 
 const getMarketRewards = async (
@@ -5,7 +6,23 @@ const getMarketRewards = async (
 ): Promise<{ marketId: number; totalRewards: string }[]> => {
   // ... implementation here
 
-  return [];
+  const baseAsset = 'Ztg';
+  const queryToFetchMarkets = gql`
+  query QueryMarkets($address: String!) {
+    markets(where: {creator_eq: $address, baseAsset_eq: "Ztg", status_eq: Resolved}) {
+      marketId,
+      baseAsset
+    }
+  }`;
+
+  const variables = {
+    address: address,
+    baseAsset: baseAsset,
+  };
+
+  const marketData = await client.request(queryToFetchMarkets, variables);
+
+  return marketData;
 };
 
 export default getMarketRewards;
